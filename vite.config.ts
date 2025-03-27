19c351e205a0ca6e0c67509f7097705a9fffaeae
Mcmarsh <Mcmarsh.fif@gmail.com>
2025-03-27 17:59:21 +0200

    revert to original working

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: true,
+    host: true, // This allows connections from all network interfaces
     port: Number(process.env.PORT) || 3000,
   },
   preview: {
